58bc969d91dd14d0feaf7491e92a345fef23d0c0
tianc03 <tianc03#163.com>
2023-05-23 17:21:58 +0000

    exp

---
+++ exp.py
@@ -0,0 +1,58 @@
+import os,pickle
+
+filename="rot-cos-path1-disbuf-grapht_ind-rely_gen_one-l1000x0.1mx300-tr9-noinv-R4+.out"
+#filename="cos-path1-disbuf-grapht_ind-rely_gen_one-l1000x0.1mx200-tr9-noinv-R4+.out"
+with open(filename) as f:
+    lines = f.readlines()
+rule_quality = dict()
+Vmrr = dict()
+Tmrr = dict()
+VH1 = dict()
+TH1 = dict()
+roundnum = 10
+Vsmp = 0 #样本数
+Tsmp = 0 #样本数
+for i in range(roundnum):
+    rule_quality[i] = 0
+    Vmrr[i] = 0
+    Tmrr[i] = 0
+    VH1[i] = 0
+    TH1[i] = 0
+
+#各回合的rule_quality
+rind = 0
+count_ind = 0
+tmp_qual = 0
+for line in lines:
+    inf = line.strip().split(',')
+    if len(inf)==4:
+        #print(inf)
+        if inf[1][:6]=='Top 20':
+            res = inf[1].split(' = ')
+            #tmp_qual = float(res[1])
+            rule_quality[count_ind] += float(res[1])
+    if "__V__" in line:
+        inftest = line.strip().split('__V__')
+        inftest = inftest[1].split('\t')
+        Vnum = int(inftest[2])
+        Vmrr[count_ind] += float(inftest[4])*Vnum
+        VH1[count_ind] += float(inftest[5])*Vnum
+    
+    if "__T__" in line:
+        inftest = line.strip().split('__T__')
+        inftest = inftest[1].split('\t')
+        Tnum = int(inftest[2])
+        Tmrr[count_ind] += float(inftest[4])*Tnum
+        TH1[count_ind] += float(inftest[5])*Tnum
+        count_ind += 1
+
+    
+    if count_ind == roundnum:
+        count_ind = 0 
+        rind += 1
+        Vsmp += Vnum
+        Tsmp += Tnum
+        for i in range(roundnum):
+            dat = '\t'.join([str(rind),str(i),str(rule_quality[i]/rind),str(Vmrr[i]/Vsmp),str(Tmrr[i]/Tsmp),str(VH1[i]/Vsmp),str(TH1[i]/Tsmp)])
+            #print(f"{rind,i},rule_quality,{dat}")
+            print(dat)
